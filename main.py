079b41603d9dee48ae4322c39d7d74f87b38c4ea
Rafael A. Flores M <rflores@activeit.cl>
2023-04-12 00:15:51 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+from fastapi import FastAPI,Request, Response
+from fastapi.responses import JSONResponse
+from BodyRequest import *
+from langchain import OpenAI, ConversationChain
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
+
+app = FastAPI()
+session_state = {}
+
+OPENAI_API_KEY = "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"
+
+llm = OpenAI(
+        temperature=0,
+        openai_api_key=OPENAI_API_KEY
+    )
+
+Conversation = ConversationChain(
+                    llm=llm,
+                    verbose=True,
+                    memory=ConversationBufferMemory()
+                )
+
+
+
+@app.get('/')
+async def index(request: Request) -> Response:
+    return JSONResponse(content={"msg":"Welcome to chat-bot API"}, status_code=200)
+
+
+@app.post('/chat-bot')
+async def chatBot(request: Request) -> Response:
+
+    jsonIncoming = await request.json()
+    items = ItemBodyRequest(** jsonIncoming)
+
+    if items.session_id not in session_state:
+        session_state[items.session_id] = ConversationBufferMemory()
+
+    Conversation.memory = session_state[items.session_id]
+
+    return Conversation.run(input=items.message)
+
+
+
+
